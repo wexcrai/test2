caf198835df4f556094053b5306952fa22d6d27a
wexcrai <prob23276@gmail.com>
2026-05-16 15:14:19 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -243,9 +243,9 @@ function AppContent() {
               </div>
             )}
 
-            {messages.length === 0 && !isSending ? (
-              <WelcomeScreen />
-            ) : (
+           {messages.length === 0 && !isSending ? (
+  <WelcomeScreen onSuggestion={(text) => handleSend(text)} />
+) : (
               <div className="flex-1 overflow-y-auto p-4 space-y-4">
                 {messages.map((msg, index) => (
                   <ChatMessage
